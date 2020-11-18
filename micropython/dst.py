656458b7e0a1ccb88f225bd40da8d97037292bdf
Emard <vordah@gmail.com>
2020-11-18 17:44:34 +0100

    dst.py: at october, switch DST at 00:00 GMT for clocks to catch change at 02:00 local time

--- micropython/dst.py
+++ micropython/dst.py
@@ -4,9 +4,9 @@ def is_dst(t:int):
   year = time.localtime(t)[0] # get current year
   # wd: 0-sunday .. 6-saturday
   wd_end_march   = (time.localtime(time.mktime((year, 3,31,1,0,0,0,0,0)))[6]+1)%7
-  wd_end_october = (time.localtime(time.mktime((year,10,31,1,0,0,0,0,0)))[6]+1)%7
+  wd_end_october = (time.localtime(time.mktime((year,10,31,0,0,0,0,0,0)))[6]+1)%7
   dst_on  = time.mktime((year, 3,31-wd_end_march  ,1,0,0,0,0,0)) # last sunday of March, DST on
-  dst_off = time.mktime((year,10,31-wd_end_october,1,0,0,0,0,0)) # last sunday of October, DST off
+  dst_off = time.mktime((year,10,31-wd_end_october,0,0,0,0,0,0)) # last sunday of October, DST off
   # between last sunday of March and last sunday of October
   return t >= dst_on and t < dst_off
 
