ce81f366d5b73a10d1c7e850dd1e9fd0c5b0fe61
Emard <vordah@gmail.com>
2020-11-18 16:44:19 +0100

    dst.py remove unused command

--- micropython/dst.py
+++ micropython/dst.py
@@ -7,7 +7,6 @@ def is_dst(t:int):
   wd_end_october = (time.localtime(time.mktime((year,10,31,1,0,0,0,0,0)))[6]+1)%7
   dst_on  = time.mktime((year, 3,31-wd_end_march  ,1,0,0,0,0,0)) # last sunday of March, DST on
   dst_off = time.mktime((year,10,31-wd_end_october,1,0,0,0,0,0)) # last sunday of October, DST off
-  now=time.time()
   # between last sunday of March and last sunday of October
   return t >= dst_on and t < dst_off
 
